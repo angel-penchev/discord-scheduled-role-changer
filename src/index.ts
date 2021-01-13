c91eb9d54fd9f371a521986aa9561edfb9badb9c
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-01-13 19:22:39 +0200

    [Test] Bot CI
    Created a workflow to check whether the build is all valid or not.

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ client.on('ready', () => console.log(`${client.user?.username} has started.`));
 
 client.on('message', (message) => {
   if (message.author.bot) return;
-  // if(!message.guild).hasPermission('MANAGE_ROLES')) return;
 
   // !bs rename - Schedules a role rename
   if (message.content.startsWith(config.discordCommandPrefix + ' rename')) {
